3571a835330795fed40d2aebe71c0b3376f69a68
Mark van den Boomen <koenijn@gmail.com>
2012-02-01 19:28:32 +0100

    added example input with actionbutton

--- ReusableWebComponents/src/main/java/nl/vandenboomen/tryouts/reusablewebcomponents/bean/CompositeComponentsBean.java
+++ ReusableWebComponents/src/main/java/nl/vandenboomen/tryouts/reusablewebcomponents/bean/CompositeComponentsBean.java
@@ -5,8 +5,11 @@
 package nl.vandenboomen.tryouts.reusablewebcomponents.bean;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 /**
  *
@@ -17,10 +20,15 @@ import javax.faces.bean.ViewScoped;
 public class CompositeComponentsBean {
 
     private String yourName;
-
+    private int popupTriggerCount = 0;
+    
     @PostConstruct
     private void init() {
         yourName = "default name";
+    }
+    
+    public void popupAction(ActionEvent event) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("The popupbutton has been triggered " + ++popupTriggerCount + " times!"));
     }
     
     public String getYourName() {
