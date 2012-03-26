388961875369f16a04925f027cd75ae713bc66fb
Mark van den Boomen <koenijn@gmail.com>
2012-03-26 22:13:05 +0200

    Added example of programmaticaly adding standard JSF components

--- ReusableWebComponents/src/main/java/nl/vandenboomen/tryouts/reusablewebcomponents/bean/CompositeComponentsBean.java
+++ ReusableWebComponents/src/main/java/nl/vandenboomen/tryouts/reusablewebcomponents/bean/CompositeComponentsBean.java
@@ -5,9 +5,13 @@
 package nl.vandenboomen.tryouts.reusablewebcomponents.bean;
 
 import javax.annotation.PostConstruct;
+import javax.el.ELContext;
+import javax.el.ValueExpression;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlInputText;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
@@ -25,6 +29,20 @@ public class CompositeComponentsBean {
     @PostConstruct
     private void init() {
         yourName = "default name";
+        
+        // begin programmaticaly add component
+        UIComponent component = FacesContext.getCurrentInstance().getViewRoot().findComponent(":myForm:programmaticContainer");
+        
+        UIComponent inputStuff = FacesContext.getCurrentInstance().getApplication().createComponent(HtmlInputText.COMPONENT_TYPE);
+        HtmlInputText realInput = (HtmlInputText) inputStuff;
+        
+        ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+        ValueExpression nameExpression = FacesContext.getCurrentInstance().getApplication().getExpressionFactory().createValueExpression(elContext, "#{compositeComponentsBean.yourName}", String.class);
+        
+        realInput.setValueExpression("value", nameExpression);
+        
+        component.getChildren().add(inputStuff);
+        // end programmaticaly add component
     }
     
     public void popupAction(ActionEvent event) {
